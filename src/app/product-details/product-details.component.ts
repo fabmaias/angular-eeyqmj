0eb96a7155c796f4fead64fb4818294e270e34e0
fabmaias <fabmaias@gmail.com>
2020-03-14 16:34:07 -0300

    2 files modified, 1 added

--- src/app/product-details/product-details.component.ts
+++ src/app/product-details/product-details.component.ts
@@ -9,11 +9,13 @@ import { products } from '../products';
   styleUrls: ['./product-details.component.css']
 })
 export class ProductDetailsComponent implements OnInit {
+  products = products;
   product;
 
   constructor(private route: ActivatedRoute) { }
 
   ngOnInit() {
+    
     this.route.paramMap.subscribe(params => {
     this.product = products[+params.get('productId')];
   });
